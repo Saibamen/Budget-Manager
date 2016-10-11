564dfcb38e26ab7b3d80a4c5f648865351d2c517
Adam Stachowicz <a.stachowicz@praktyki.itmation.pl>
2016-10-11 20:49:34 +0200

    Nie zmieniaj wartości, jeśli wybrano placeholder i zmieniaj tylko wtedy, jeśli znaleziono dane

--- public/js/changefieldsvalue.js
+++ public/js/changefieldsvalue.js
@@ -1,12 +1,16 @@
 $(document).ready(function() {
     $("#source_id").change(function() {
-        $("#ajax-loading").show();
+        if($("#source_id").val()) {
+            $("#ajax-loading").show();
 
-        $.getJSON($("#source_id").data("url") + $("#source_id").val(), function(data) {
-            $("#type_id").val(data[0].type_id);
-            $("#value").val(data[0].value);
+            $.getJSON($("#source_id").data("url") + $("#source_id").val(), function(data) {
+                if(data[0]) {
+                    $("#type_id").val(data[0].type_id);
+                    $("#value").val(data[0].value);
+                }
 
-            $("#ajax-loading").hide();
-        });
+                $("#ajax-loading").hide();
+            });
+        }
     })
 });
